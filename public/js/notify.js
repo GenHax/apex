58f553e4f4b4e62d73c326ab440af3bf18fe235e
rkpattnaik780 <rkpattnaik780@gmail.com>
2018-08-19 11:08:25 +0530

    Messaging text modified.

--- public/js/notify.js
+++ public/js/notify.js
@@ -9,10 +9,9 @@ $(document).ready(function () {
         console.log("here");
         console.log(position.coords.latitude);
         console.log(position.coords.longitude);
-        map_url = "https://www.google.com/maps/@" + position.coords.latitude + "," + position.coords.longitude + ",15z" ;
+        map_url = "The location of the patient is : " + " https://www.google.com/maps/@" + position.coords.latitude + "," + position.coords.longitude + ",15z"  + "\nName : Maitree \nContact : 876542087 \nAddress : Plot 35 , Surya Nagar";
         map_url = encodeURI(map_url);
     });
-
     $("#notification-btn").click(function () {
 
          $.get("http://api.msg91.com/api/sendhttp.php?sender=MSGIND&route=4&mobiles=7381594387&authkey=232467ACIKcpWuJmw5b7820fa&country=91&message=" + map_url , function (data, status) {
